8f69222220f1d94f27fe4f58bdf095a2d1819b3b
DurgaSankar <durgasankarpothal@gmail.com>
2025-07-07 13:45:47 +0530

    dev file

--- src/main/java/durgaproject/jornalapp1/entity/JornalEntry.java
+++ src/main/java/durgaproject/jornalapp1/entity/JornalEntry.java
@@ -1,19 +1,11 @@
 package durgaproject.jornalapp1.entity;
-
-
 import durgaproject.jornalapp1.Enum.Sentiment;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
-
 @Document(collection = "jornalEntry")
 @Data
 @NoArgsConstructor
