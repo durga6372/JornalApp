a9789ec1211a5f598bf08223970d0b89ef2d00fc
DurgaSankar <durgasankarpothal@gmail.com>
2025-07-15 16:03:22 +0530

    fix: removed sensitive files and updated configuration

--- jornalapp1/jsfolder/first.js
+++
@@ -1,5 +0,0 @@
-console.log("hello world!");
-let num=2+2
-console.log(num)
-let username="Durga"
-console.log(username)
--- src/main/java/durgaproject/jornalapp1/dto/UserDTO.java
+++ src/main/java/durgaproject/jornalapp1/dto/UserDTO.java
@@ -5,9 +5,6 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
